e33eafdf8d8facc065aed78d28b6dd5c3edab513
rrhoades10 <rrhoades10@yahoo.com>
2021-09-02 11:34:45 -0500

    Commit for Herokue Deployment

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+# Give access to the project in ANY OS we find ourselves in
+# Allow outside filses/folders to be added to the project from the
+# Base directory
+
+
+class Config():
+    """
+        Set Config variables for the flask app.
+        Using Evironment variables where available otherwise
+        create the config variables if not done already.
+    """
+    # FLASK_APP = os.environ.get('FLASK_APP')
+    # FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "You will never guess"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Turn off Update Messages from the sqlalchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
